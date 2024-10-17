15da7b9975ac24c90bd067656eea9098a07ba0d4
Tatyana Bondar <sinopahfox@yandex.ru>
2024-10-17 12:58:55 +0300

    feat (main): add files lab05

---
+++ labs/lab05/lab5-2.asm
@@ -0,0 +1,15 @@
+; Программа вывода сообщений на экран и ввода с клавиатуры
+%include 'in_out.asm' ; подключение внешнего файла
+SECTION .data ; Секция инициированных данных
+msg: DB 'Введите строку:',0h ;сообщение 
+SECTION .bss ; секция неинициированных данных 
+buf1: RESB 80 ;буфер размером 80 байт
+SECTION .text ; код программы
+GLOBAL _start ; начало программы
+_start:     ; точка входа в программу
+    mov eax, msg
+    call sprint
+    mov ecx, buf1
+    mov edx, 80
+    call sread
+    call quit
